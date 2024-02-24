7f32e5b393d1c3e240b72b238f16b9f509254390
Zoe <zmb6893@g.rit.edu>
2024-02-24 17:29:09 -0500

    Added tailwinf and stackBar

--- passive-aggressive-plant/app/components/Home.svelte
+++ passive-aggressive-plant/app/components/Home.svelte
@@ -1,13 +1,18 @@
 <page>
-    <actionBar title="Home" />
-    <gridLayout>
-        <label class="info">
-            <formattedString>
-                <span class="fas" text="&#xf135;" />
-                <span text=" {message}" />
-            </formattedString>
-        </label>
-    </gridLayout>
+    <actionBar>
+        <stackLayout orientation="horizontal">
+            <image src="res://icon" width="40" height="40" verticalAlignment="center" />
+            <label text="NativeScript" fontSize="24" verticalAlignment="center" />
+        </stackLayout>
+    </actionBar>
+    <stackLayout backgroundColor="#3c495e">
+        <dockLayout>
+            <label text="left" height="70%" backgroundColor="#4383b8"/>
+            <label text="right" height="70%" backgroundColor="#4383b8"/>
+        </dockLayout>
+        
+        <label text="second" height="30%" backgroundColor="#286290"/>
+    </stackLayout>
 </page>
 
 <script lang="ts">
